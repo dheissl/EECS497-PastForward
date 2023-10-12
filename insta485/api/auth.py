8b9aaca324fed00c509597fe0d66b069c610c654
gaven <gaven@umich.edu>
2023-10-12 12:17:32 -0400

    likespost.py (likes/?=postid/) and auth.py in progress

---
+++ insta485/api/auth.py
@@ -0,0 +1,31 @@
+"""Authentification."""
+import flask
+import insta485
+
+def get_auth(username, password):
+    connection = insta485.model.get_db()
+
+    cred = connection.execute(
+        "SELECT username, password "
+        "FROM users "
+        "WHERE username = ? ",
+        (username, )
+    )
+    credentials = cred.fetchone()
+
+    if not credentials:
+        return -1
+
+    db_password = credentials['password']
+    algorithm, salt, hash = db_password.split('$')
+
+    hash_obj = hashlib.new(algorithm)
+    password_salted = salt + password
+    hash_obj.update(password_salted.encode('utf-8'))
+    password_hash = hash_obj.hexdigest()
+    password_string = "$".join([algorithm, salt, password_hash])
+
+    if db_password != password_string:
+        return -2
+    
+    return 0
