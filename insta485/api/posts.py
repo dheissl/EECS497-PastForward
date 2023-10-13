e1e6f6f7e764474aff347ef0e493aa22a6ce61d9
dheiss <dheiss@umich.edu>
2023-10-13 07:29:13 -0400

    fixed posts

--- insta485/api/posts.py
+++ insta485/api/posts.py
@@ -1,37 +1,79 @@
 """REST API for posts."""
 import flask
 import insta485
-
+from insta485.api.auth import get_auth
 
 @insta485.app.route('/api/v1/posts/')
 def get_post():
     """Return post on postid."""
+    params = []
+    if flask.request.args.get("size", type=int) is not None:
+      size = flask.request.args.get("size", default=10, type=int)  # Default to 10 if not provided
+      params.append(f"size={size}")
+    else:
+      size = 10
+    if flask.request.args.get("page", type=int) is not None:
+      page = flask.request.args.get("page", default=0, type=int)  # Default to 0 if not provided
+      params.append(f"page={page}")
+    else:
+      page = 0
+    if flask.request.args.get('postid_lte', type=int) is not None:
+      postid_lte = flask.request.args.get('postid_lte', type=int, default=None)  # Default to None if not provided
+      params.append(f"postid_lte={postid_lte}")
+    else:
+      postid_lte = None
 
-    size = flask.request.args.get("size", default=10, type=int)  # Default to 10 if not provided
-    page = flask.request.args.get("page", default=0, type=int)  # Default to 0 if not provided
-    postid_lte = flask.request.args.get('postid_lte', type=int, default=None)  # Default to None if not provided
     if size <= 0:
       return flask.Response('BAD REQUEST', 400)
     if page < 0:
       return flask.Response('BAD REQUEST', 400)
     #todo this being a bitch cant authorize
-    username = ""
-    if hasattr(flask, 'session'):
-      if 'username' in flask.session:
-        username = flask.session['username']
-      else:
-        return flask.Response('Could not verify', 403)
-    # Todo: change this to account for each username
-    elif 'username' in flask.request.authorization:
-      username = flask.request.authorization['username']
-      password = flask.request.authorization['password']
-      if not verify_user(username, password):
-        return flask.Response('Could not verify', 403)
-      pass
+    logname = ""
+    
+    if 'logname' not in flask.session:
+        username = flask.request.authorization['username']
+        password = flask.request.authorization['password']
+        if username is not None:
+            context = {}
+            # -1 represents username does not exist
+            if (get_auth(username, password) == -1):
+                context["message"] = "Username not found"
+                context["status_code"] = 403
+                return flask.jsonify(**context), 403
+            
+            # -2 represents wrong password
+            elif (get_auth(username, password) == -2):
+                context["message"] = "Incorrect Password"
+                context["status_code"] = 403
+                return flask.jsonify(**context), 403
+            else:
+                logname = username
+        else:
+            return flask.redirect(url_for('show_login'))
     else:
-      return flask.Response('Could not verify', 403)
-      
+        logname = flask.session['logname']
+    username = logname
+
     connection = insta485.model.get_db()
+    cur = connection.execute(
+      "SELECT p.filename AS pic, p.owner AS owner, p.postid AS postid,"
+      " p.created AS timestamp, u.filename AS profile_pic "
+      "FROM posts AS p "
+      "JOIN following AS f ON p.owner = f.username2 "
+      "JOIN users AS u ON p.owner = u.username "
+      "WHERE f.username1 = ? "
+      "AND p.postid > ? "
+      "UNION "
+      "SELECT p.filename AS pic, p.owner AS owner, p.postid AS postid,"
+      " p.created AS timestamp, u.filename AS profile_pic "
+      "FROM posts AS p "
+      "JOIN users AS u ON p.owner = u.username "
+      "WHERE p.owner = ? "
+      "AND p.postid > ?",
+      (username, postid_lte, username, postid_lte)
+    )
+    newposts = cur.fetchall()
+
     cur = connection.execute(
         "SELECT p.filename AS pic, p.owner AS owner, p.postid AS postid,"
         " p.created AS timestamp, u.filename AS profile_pic "
@@ -48,25 +90,29 @@ def get_post():
         "ORDER BY p.postid DESC"
         " LIMIT ? "
         "OFFSET ?",
-        (username, username, size, page*size)
+        (username, username, size, (page*size)+len(newposts))
     )
     posts = cur.fetchall()
     if postid_lte is None:
       postid_lte = posts[0]["postid"]
     page = page + 1
     nex = ""
-    lenth = length(posts)
+    lenth = len(posts)
     if lenth >= size:
       nex = f"/api/v1/posts/?size={size}&page={page}&postid_lte={postid_lte}"
     else:
       size = lenth
 
+    base_url = f"/api/v1/posts/"
+    if params:
+      base_url = f"{base_url}?{'&'.join(params)}"
 
-    result = {"next": nex, "results": [], "url": "/api/v1/posts/"}
+    result = {"next": nex, "results": [], "url": base_url}
     for i in range(size):
+      print('YOo')
       result["results"].append({"postid": posts[i]["postid"], 
                           "url": f"/api/v1/posts/{posts[i]['postid']}/"})
-    
+    print(result["results"])
     return result
 
 def verify_user(username, password):
