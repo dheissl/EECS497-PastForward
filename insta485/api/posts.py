25e47e4b6b870038c64e0558ef6a674f2199932c
gaven <gaven@umich.edu>
2023-10-14 09:38:11 -0400

    auth fixed for posts.py, passing auth tests

--- insta485/api/posts.py
+++ insta485/api/posts.py
@@ -49,7 +49,7 @@ def get_posts():
             else:
                 logname = username
         else:
-            return flask.redirect(flask.url_for('show_login'))
+            return flask.redirect(flask.url_for('show_login')), 403
     else:
         logname = flask.session['logname']
     username = logname
