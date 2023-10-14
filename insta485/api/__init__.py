80552d54aa17cf2beaa5d7453ca42957535a398d
gaven <gaven@umich.edu>
2023-10-14 01:16:35 -0400

    resolving conflicts

--- insta485/api/__init__.py
+++ insta485/api/__init__.py
@@ -3,3 +3,6 @@ from insta485.api.index import get_index
 from insta485.api.posts import get_posts
 from insta485.api.likespost import get_like_create
 from insta485.api.postinfo import get_post
+from insta485.api.likesdelete import get_like_delete
+from insta485.api.likespost import get_like_create
+from insta485.api.comments import get_comment_create
---
+++ insta485/api/likesdelete.py
@@ -0,0 +1,64 @@
+"""REST API for likes create."""
+import flask
+import insta485
+from insta485.api.auth import get_auth
+
+
+@insta485.app.route('/api/v1/likes/<likeid>/', methods=['DELETE'])
+def get_like_delete(likeid):
+    """Delete one “like”. Return 204 on success. If the likeid does not exist,
+     return 404. If the user does not own the like, return 403."""
+
+    logname = ""
+
+    if 'logname' not in flask.session:
+        username = flask.request.authorization['username']
+        password = flask.request.authorization['password']
+        if username is not None:
+            context = {}
+            # -1 represents username does not exist
+            if (get_auth(username, password) == -1):
+                context["message"] = "Username not found"
+                context["status_code"] = 403
+                return flask.jsonify(**context), 403
+            
+            # -2 represents wrong password
+            elif (get_auth(username, password) == -2):
+                context["message"] = "Incorrect Password"
+                context["status_code"] = 403
+                return flask.jsonify(**context), 403
+            else:
+                logname = username
+        else:
+            return flask.redirect(url_for('show_login'))
+    else:
+        logname = flask.session['logname']
+
+    connection = insta485.model.get_db()
+    lik = connection.execute(
+        "SELECT owner, likeid "
+        "FROM likes "
+        "WHERE likeid = ?",
+        (likeid,)
+    )
+    like = lik.fetchone()
+
+    context = {}
+    if like is None:
+        context["message"] = "likeid does not exist"
+        context["status_code"] = 406
+        return flask.jsonify(**context), 404
+    elif (like["owner"] != logname):
+        context["message"] = "user does not own the like"
+        context["status_code"] = 403
+        return flask.jsonify(**context), 403
+    else:
+        connection.execute(
+            "DELETE FROM likes "
+            "WHERE owner = ? AND likeid = ?",
+            (logname, likeid,)
+        )
+        
+        context["message"] = "delete like with likeid = {{likeid}}"
+        context["status_code"] = 204
+        return flask.jsonify(**context), 204
