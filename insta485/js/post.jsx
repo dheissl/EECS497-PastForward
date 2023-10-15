2336ae58d7e89844b94bbcfe0e69431ddfdb60d9
gaven <gaven@umich.edu>
2023-10-15 12:46:29 -0400

    adasfsa

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -8,6 +8,10 @@ export default function Post({ url }) {
 
   const [imgUrl, setImgUrl] = useState("");
   const [owner, setOwner] = useState("");
+  const [lognameLikesThis, setLognameLikesThis] = useState(false);
+  const [numLikes, setNumLikes] = useState(0);
+  const [likesUrl, setLikesUrl] = useState("");
+  const [postId, setPostId] = useState(0);
 
   useEffect(() => {
     // Declare a boolean flag that we can use to cancel the API request.
@@ -25,6 +29,10 @@ export default function Post({ url }) {
         if (!ignoreStaleRequest) {
           setImgUrl(data.imgUrl);
           setOwner(data.owner);
+          setLognameLikesThis(data.likes.lognameLikesThis);
+          setNumLikes(data.likes.numLikes);
+          setLikesUrl(data.likes.url);
+          setPostId(data.postid);
         }
       })
       .catch((error) => console.log(error));
@@ -36,12 +44,67 @@ export default function Post({ url }) {
       ignoreStaleRequest = true;
     };
   }, [url]);
+  
+    const likeButton = () => {
+        // Make an API call to like/unlike the post
+        // You will need to implement this API endpoint on your server.
+        if (lognameLikesThis) {
+            fetch(likesUrl, {method: "DELETE", credentials: "same-origin",})
+                .then((response) => {
+                if (!response.ok) throw Error(response.statusText);
+                return response.json();
+                })
+                .catch((error) => console.log(error));
+            
+            setLognameLikesThis(!lognameLikesThis);
+            setNumLikes(numLikes - 1);
+        } else {
+            fetch("/api/v1/likes/?postid=${postid}", {method: "POST", credentials: "same-origin",})
+                .then((response) => {
+                if (!response.ok) throw Error(response.statusText);
+                return response.json();
+                })
+                .then((data) => {
+                    if (!ignoreStaleRequest) {
+                        setLikesUrl(data.url);
+                    }
+                })
+                .catch((error) => console.log(error));
+                
+            setLognameLikesThis(!lognameLikesThis);
+            setNumLikes(numLikes + 1);
+                
+        }
+      };
+      
+      const doubleClick = () => {
+        if (!lognameLikesThis) {
+            fetch("/api/v1/likes/?postid={postid}", {credentials: "same-origin",})
+                .then((response) => {
+                if (!response.ok) throw Error(response.statusText);
+                return response.json();
+                })
+                .then((data) => {
+                    if (!ignoreStaleRequest) {
+                        setLikesUrl(data.url);
+                    }
+                })
+                .catch((error) => console.log(error));
+                
+            setLognameLikesThis(!lognameLikesThis);
+            setNumLikes(numLikes + 1);
+        }
+}
 
   // Render post image and post owner
   return (
     <div className="post">
-      <img src={imgUrl} alt="post_image" />
-      <p>{owner}</p>
+        <img src={imgUrl} alt="post_image" onDoubleClick={() => doubleClick()} />
+        <p>{owner}</p>
+        <button data-testid="like-unlike-button" onClick={likeButton}>
+           {lognameLikesThis ? 'Unlike' : 'Like'}
+        </button>
+        <p>{numLikes === 1 ? '1 like' : `${numLikes} likes`}</p>
     </div>
   );
 }
