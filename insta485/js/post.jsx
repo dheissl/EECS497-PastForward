6ee40ac1f26ac889c5ea2c151f6efc39007a7613
gaven <gaven@umich.edu>
2023-10-15 15:06:43 -0400

    asldkfalsf

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
+
 
 // The parameter of this function is an object with a string called url inside it.
 // url is a prop for the Post component.
@@ -8,10 +15,13 @@ export default function Post({ url }) {
 
   const [imgUrl, setImgUrl] = useState("");
   const [owner, setOwner] = useState("");
+  const [time, setTime] = useState("");
   const [lognameLikesThis, setLognameLikesThis] = useState(false);
   const [numLikes, setNumLikes] = useState(0);
   const [likesUrl, setLikesUrl] = useState("");
   const [postId, setPostId] = useState(0);
+  const [comments, setComments] = useState([]);
+  const[text, setText] = useState("");
 
   useEffect(() => {
     // Declare a boolean flag that we can use to cancel the API request.
@@ -29,10 +39,12 @@ export default function Post({ url }) {
         if (!ignoreStaleRequest) {
           setImgUrl(data.imgUrl);
           setOwner(data.owner);
+          setTime(dayjs(data.created).fromNow());
           setLognameLikesThis(data.likes.lognameLikesThis);
           setNumLikes(data.likes.numLikes);
           setLikesUrl(data.likes.url);
           setPostId(data.postid);
+          setComments(data.comments);
         }
       })
       .catch((error) => console.log(error));
@@ -77,7 +89,7 @@ export default function Post({ url }) {
         }
       };
       
-      const doubleClick = () => {
+    const doubleClick = () => {
         if (!lognameLikesThis) {
             fetch(`/api/v1/likes/?postid=${postId}`, {method: "POST", credentials: "same-origin",})
                 .then((response) => {
@@ -94,17 +106,82 @@ export default function Post({ url }) {
             setLognameLikesThis(!lognameLikesThis);
             setNumLikes(numLikes + 1);
         }
-}
+    };
+        
+    const commentButton = (commentId) => {
+        fetch(`/api/v1/comments/${commentId}`, {method: "DELETE",
+            credentials: "same-origin",})
+            .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+            })
+            .then(() => {
+                setComments(comments.filter((comment) => comment.commentid !== commentId));
+            })
+            .catch((error) => console.log(error));
+            
+    };
+    
+    const enter = (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addComment();
+        }
+    };
+        
+    const addComment = () => {
+        const new_comment = {};
+        
+        fetch(`/api/v1/comments/?postid=${postId}`, {method: "POST",
+            credentials: "same-origin",
+             headers: {"Content-Type": "application/json",},
+              body: JSON.stringify({text}),})
+            .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+            })
+            .then((data) => {
+                if (!ignoreStaleRequest) {
+                    new_comment = data;
+                }
+            })
+            .catch((error) => console.log(error));
+        
+        setComments([...comments, new_comment])
+        setText("");
+    };
+
 
   // Render post image and post owner
   return (
     <div className="post">
         <img src={imgUrl} alt="post_image" onDoubleClick={() => doubleClick()} />
         <p>{owner}</p>
+        <p>{time}</p>
         <button data-testid="like-unlike-button" onClick={likeButton}>
            {lognameLikesThis ? 'Unlike' : 'Like'}
         </button>
         <p>{numLikes === 1 ? '1 like' : `${numLikes} likes`}</p>
+        <div className="comments">
+        {comments.map((comment) => (
+          <div key={comment.commentid}>
+            <p>{comment.owner}</p>
+            <span data-testid="comment-text">{comment.text}</span>
+            {comment.lognameOwnsThis && (
+                <button data-testid="delete-comment-button" onClick={() =>
+                    commentButton(comment.commentid)}>
+                    Delete Comment
+                </button>
+            )}
+          </div>
+        ))}
+        <form data-testid="comment-form" onSubmit={e => {e.preventDefault();
+                alert('Submitting!'); addComment()}}>
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)}
+                onKeyPress={enter} placeholder="Add a comment..." />
+        </form>
+        
+      </div>
     </div>
   );
 }
