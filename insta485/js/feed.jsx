af4c4654bb94edd0ba1945b665861df01d3b23f2
dheiss <dheiss@umich.edu>
2023-10-15 16:26:04 -0400

    fsfsFS

--- insta485/js/feed.jsx
+++ insta485/js/feed.jsx
@@ -5,7 +5,6 @@ import InfiniteScroll from "react-infinite-scroll-component";
 export default function Feed() {
   const [posts, setPosts] = useState([]);
   const [nextPageUrl, setNextPageUrl] = useState("/api/v1/posts/");
-  const [initialFetchComplete, setInitialFetchComplete] = useState(false);
 
   const fetchMoreData = () => {
     if (!nextPageUrl) return; // No more posts to fetch
@@ -16,34 +15,32 @@ export default function Feed() {
         return response.json();
       })
       .then(data => {
-        if (!initialFetchComplete) {
-          // If initial fetch, replace posts with data.results
-          setPosts(data.results);
-          setInitialFetchComplete(true);
-        } else {
-          // If not initial fetch, append to existing posts
-          setPosts(prevPosts => [...prevPosts, ...data.results]);
-        }
+        setPosts(prevPosts => [...prevPosts, ...data.results]);
         setNextPageUrl(data.next);
       })
       .catch(error => console.log(error));
   };
 
-  useEffect(() => {
-    fetchMoreData();
-  }, []);
+    // Initial data fetch on mount
+    useEffect(() => {
+        fetch("/api/v1/posts/", { credentials: "same-origin" })
+        .then(response => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+        })
+        .then(data => {
+            setPosts(data.results);
+            setNextPageUrl(data.next);
+        })
+        .catch(error => console.log(error));
+    }, []);  // Empty dependency array means this will only run once on mount
+    
 
   return (
     <InfiniteScroll
       dataLength={posts.length}
       next={fetchMoreData}
       hasMore={nextPageUrl !== null} // Fetch more only if there's a next page
-      loader={<h4>Loading...</h4>}
-      endMessage={
-        <p style={{ textAlign: "center" }}>
-          <b>End of feed</b>
-        </p>
-      }
     >
       {posts.map(post => (
         <Post key={post.postid} url={post.url} />
